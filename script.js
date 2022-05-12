2aadbafa9bcef4fb004c1551fb8416ac360d6b0d
colinale <colinale@buffalo.edu>
2022-05-12 17:22:45 -0400

    added more attricutes, quiz ending, time to quiz

--- script.js
+++ script.js
@@ -57,3 +57,72 @@ function startQuiz() {
         choicesEl.appendChild(choiceNode);
     });
 }
+
+function questionClick() {
+    // check if user guessed wrong
+    if (this.value !== questions[currentQuestionIndex].answer) {
+      // penalize time
+      time -= 15;
+  
+      if (time < 0) {
+        time = 0;
+      }
+  
+      // display new time on page
+      timerEl.textContent = time;
+  
+      // play "wrong" sound effect
+      sfxWrong.play();
+  
+      feedbackEl.textContent = "Wrong!";
+    } else {
+      // play "right" sound effect
+      sfxRight.play();
+  
+      feedbackEl.textContent = "Correct!";
+    }
+  
+    // flash right/wrong feedback on page for half a second
+    feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+  
+    // move to next question
+    currentQuestionIndex++;
+  
+    // check if we've run out of questions
+    if (currentQuestionIndex === questions.length) {
+      quizEnd();
+    } else {
+      getQuestion();
+    }
+  }
+  
+  function quizEnd() {
+    // stop timer
+    clearInterval(timerId);
+  
+    // show end screen
+    var endScreenEl = document.getElementById("end-screen");
+    endScreenEl.removeAttribute("class");
+  
+    // show final score
+    var finalScoreEl = document.getElementById("final-score");
+    finalScoreEl.textContent = time;
+  
+    // hide questions section
+    questionsEl.setAttribute("class", "hide");
+  }
+  
+  function clockTick() {
+    // update time
+    time--;
+    timerEl.textContent = time;
+  
+    // check if user ran out of time
+    if (time <= 0) {
+      quizEnd();
+    }
+  }
+  
