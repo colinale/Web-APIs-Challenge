8727c2a2ad680d3b149063de67b4786ba69e022f
colinale <colinale@buffalo.edu>
2022-05-14 14:18:45 -0400

    Final upload with changes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Web-APIs-Challenge
+During this challenge I had the opportunity to learn a lot about web api's and learn how to connect different applications together. This is one of the toughest challenges we've had so far, but with the help of my tutor, TA's, and professor I was able to overcome certain obsticles that were holding me back from progressing. I had to make multiple files in order to accomplish what I wanted to and that was a new experience as well. I was able to build a mini quiz based on the requirements. I really enjoyed this weeks challenge because it feels like were getting more into the fun stuff and are learning to make real changes other than creating a website with styling components. Learning more in depth stuff is really progressing my knowledge and helping me understand things I wasn't understanding the previous week.
+
+
+Deployed Application: 
+Github Repository: https://github.com/colinale/Web-APIs-Challenge
+
+Images: ![Screenshot of active Portfolio site](./quiz-image.png))
+
--- scores.js
+++ scores.js
@@ -1,9 +1,29 @@
 function printHighscores() {
-    // either get scores from localstorage or set to empty array
-    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
-  
-    // sort highscores in descending order
-    highscores.sort(function(a, b) {
-      return b.score - a.score;
-    });
+
+  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+  // highscores set in descending order
+  highscores.sort(function(a, b) {
+    return b.score - a.score;
+  });
+
+  highscores.forEach(function(score) {
+    // high score tags
+    var liTag = document.createElement("li");
+    liTag.textContent = score.initials + " - " + score.score;
+
+    // page display
+    var olEl = document.getElementById("highscores");
+    olEl.appendChild(liTag);
+  });
 }
+
+function clearHighscores() {
+  window.localStorage.removeItem("highscores");
+  window.location.reload();
+}
+
+document.getElementById("clear").onclick = clearHighscores;
+
+// run function 
+printHighscores();
